aead4c58ac7204093069d387d0e4043de16970d1
mar kom <m4nk9i@gmail.com>
2019-03-18 11:19:07 +0100

    socc mana 1

--- soc1/Form1.cs
+++ soc1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace soc1
 {
@@ -16,5 +17,122 @@ namespace soc1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initdb();
+
+        }
+
+        private void initdb()
+        {
+            SQLiteConnection m_dbConnection;
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+
+            string sql = "create table highscores (name varchar(20), score int)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('Me', 3000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('Myself', 6000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('And I', 9001)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "select * from highscores order by score desc";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+            Console.ReadLine();
+
+
+        }
     }
 }
+
+
+
+// http://www.altcontroldelete.pl/artykuly/c-wpf-oraz-sqlite-razem-w-jednym-projekcie/
+
+/**
+ *  https://blog.tigrangasparian.com/2012/02/09/getting-started-with-sqlite-in-c-part-one/
+ * 
+ namespace SQLiteSamples
+{
+    class Program
+    {
+        // Holds our connection with the database
+        SQLiteConnection m_dbConnection;
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+        }
+
+        public Program()
+        {
+            createNewDatabase();
+            connectToDatabase();
+            createTable();
+            fillTable();
+            printHighscores();
+        }
+
+        // Creates an empty database file
+        void createNewDatabase()
+        {
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+        }
+
+        // Creates a connection with our database file.
+        void connectToDatabase()
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+        }
+
+        // Creates a table named 'highscores' with two columns: name (a string of max 20 characters) and score (an int)
+        void createTable()
+        {
+            string sql = "create table highscores (name varchar(20), score int)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        // Inserts some values in the highscores table.
+        // As you can see, there is quite some duplicate code here, we'll solve this in part two.
+        void fillTable()
+        {
+            string sql = "insert into highscores (name, score) values ('Me', 3000)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('Myself', 6000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('And I', 9001)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        // Writes the highscores to the console sorted on score in descending order.
+        void printHighscores()
+        {
+            string sql = "select * from highscores order by score desc";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+            Console.ReadLine();
+        }
+    }
+}
+*/
