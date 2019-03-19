b93e91e88751d0847b8e2777c30c55cc9d267091
mar kom <m4nk9i@gmail.com>
2019-03-19 13:06:41 +0100

    wtrek koniec

--- soc1/Form1.Designer.cs
+++ soc1/Form1.Designer.cs
@@ -29,10 +29,8 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.listView1 = new System.Windows.Forms.ListView();
             this.pozycja = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.nazwa = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -44,6 +42,9 @@
             this.BS = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.RB = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.PKT = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -54,16 +55,8 @@
             this.panel1.Controls.Add(this.tabControl1);
             this.panel1.Location = new System.Drawing.Point(12, 45);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1027, 393);
+            this.panel1.Size = new System.Drawing.Size(1027, 459);
             this.panel1.TabIndex = 0;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 1;
             // 
             // tabControl1
             // 
@@ -72,29 +65,20 @@
             this.tabControl1.Location = new System.Drawing.Point(3, 3);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1021, 387);
+            this.tabControl1.Size = new System.Drawing.Size(1021, 453);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.listView1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1013, 361);
+            this.tabPage1.Size = new System.Drawing.Size(1013, 427);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabela";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(762, 361);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "spotkania";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // listView1
             // 
@@ -109,17 +93,90 @@
             this.BS,
             this.RB,
             this.PKT});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
             this.listView1.Location = new System.Drawing.Point(21, 16);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(717, 313);
+            this.listView1.Size = new System.Drawing.Size(717, 405);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // pozycja
+            // 
+            this.pozycja.Text = "Lp";
+            // 
+            // nazwa
+            // 
+            this.nazwa.Text = "nazwa";
+            // 
+            // RM
+            // 
+            this.RM.Text = "RM";
+            // 
+            // W
+            // 
+            this.W.Text = "W";
+            // 
+            // R
+            // 
+            this.R.Text = "R";
+            // 
+            // P
+            // 
+            this.P.Text = "P";
+            // 
+            // BZ
+            // 
+            this.BZ.Text = "BZ";
+            // 
+            // BS
+            // 
+            this.BS.Text = "BS";
+            // 
+            // RB
+            // 
+            this.RB.Text = "RB";
+            // 
+            // PKT
+            // 
+            this.PKT.Text = "PKT";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1013, 361);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "spotkania";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(275, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(808, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1051, 450);
+            this.ClientSize = new System.Drawing.Size(1051, 529);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -150,6 +207,7 @@
         private System.Windows.Forms.ColumnHeader PKT;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- soc1/Form1.cs
+++ soc1/Form1.cs
@@ -14,6 +14,34 @@ namespace soc1
 {
     public partial class Form1 : Form
     {
+        SQLiteConnection m_dbConnection;
+        int[,,] systempolski = new int[17, 9, 2]{
+            {{18,1},{2,17},{3,16},{4,15},{5,14},{6,13},{7,12},{8,11},{9,10}},
+            {{10,18},{11,9},{12,8},{13,7},{14,6},{15,5},{16,4},{17,3},{1,2}},
+            {{18,2},{3,1},{4,17},{5,16},{6,15},{7,14},{8,13},{9,12},{10,11}},
+            {{11,18},{12,10},{13,9},{14,8},{15,7},{16,6},{17,5},{1,4},{2,3}},
+            {{18,3},{4,2},{5,1},{6,17},{7,16},{8,15},{9,14},{10,13},{11,12}},
+            {{12,18},{13,11},{14,10},{15,9},{16,8},{17,7},{1,6},{2,5},{3,4}},
+            {{18,4},{5,3},{6,2},{7,1},{8,17},{9,16},{10,15},{11,14},{12,13}},
+            {{13,18},{14,12},{15,11},{16,10},{17,9},{1,8},{2,7},{3,6},{4,5}},
+            {{5,18},{6,4},{7,3},{8,2},{9,1},{10,17},{11,16},{12,15},{13,14}},
+            {{18,14},{15,13},{16,12},{17,11},{1,10},{2,9},{3,8},{4,7},{5,6}},
+            {{6,18},{7,5},{8,4},{9,3},{10,2},{11,1},{12,17},{13,16},{14,15}},
+            {{18,15},{16,14},{17,13},{1,12},{2,11},{3,10},{4,9},{5,8},{6,7}},
+            {{7,18},{8,6},{9,5},{10,4},{11,3},{12,2},{13,1},{14,17},{15,16}},
+            {{18,16},{17,15},{1,14},{2,13},{3,12},{4,11},{5,10},{6,9},{7,8}},
+            {{8,18},{9,7},{10,6},{11,5},{12,4},{13,3},{14,2},{15,1},{16,17}},
+            {{18,17},{1,16},{2,15},{3,14},{4,13},{5,12},{6,11},{7,10},{8,9}},
+            {{9,18},{10,8},{11,7},{12,6},{13,5},{14,4},{15,3},{16,2},{17,1}}
+            };
+        
+
+        private void initdb2()
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=liga.sqlite;Version=3;");
+            m_dbConnection.Open();
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -21,7 +49,43 @@ namespace soc1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           // initdb1();
+            // initdb1();
+            initdb2();
+            popliga();
+
+        }
+
+        private void popliga()
+        {
+            string sql = "select * from ligi";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                comboBox1.Items.Add(reader[0]);
+
+//                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+            //Console.ReadLine();
+            //comboBox1.Items.Add("a");
+        }
+
+        private void poptabela()
+        {
+            listView1.Items.Clear();
+            ListViewItem litem;
+            string sql = "select * from zespoly WHERE Z_LIGA='"+(comboBox1.SelectedIndex+1).ToString() +"'";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            int i = 1;
+            while (reader.Read())
+            {
+                litem = new ListViewItem(new[] { i.ToString(),reader[1].ToString() });
+                listView1.Items.Add(litem);
+                i++;
+                //listView1.Items.Add(reader[1].ToString());
+               // listView1.Items[1][2] = "f";
+            }
+                //comboBox1.Items.Add(reader[0]);
+
 
         }
 
@@ -108,6 +172,17 @@ namespace soc1
             Console.ReadLine();
             */
 
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            poptabela();
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
